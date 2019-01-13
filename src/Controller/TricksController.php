832a8c352aa4e9993aa635dc2fd64cabe1269b05
StarbugStone <pint2stone@gmail.com>
2019-01-13 10:28:35 +0100

    forgot a double quote in route. Fix

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class TricksController extends AbstractController
 {
     /**
-     * @Route("/", name="home)
+     * @Route("/", name="home")
      */
     public function index()
     {
