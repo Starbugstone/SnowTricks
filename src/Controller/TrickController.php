428a5a0dfb9dd67c340168d66d17b195823e8ca4
StarbugStone <pint2stone@gmail.com>
2019-01-17 23:57:13 +0100

    Adding hte search page. need some JS magic and links

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -17,23 +17,22 @@ class TrickController extends AbstractController
         ]);
     }
 
-
-    /**
-     * @Route("/trick/all", name="trick.showall")
-     */
-    public function showAll()
-    {
-        return $this->render('trick/showall.html.twig', [
-            'controller_name' => 'TricksController',
-        ]);
-    }
-
     /**
      * @Route("/trick/{id}", name="trick.show")
      */
     public function show($id) //todo: change ID to slug
     {
         return $this->render('trick/show.html.twig', [
+            'controller_name' => 'TricksController',
+        ]);
+    }
+
+    /**
+     * @Route("/search", name="trick.search")
+     */
+    public function search()
+    {
+        return $this->render('trick/search.html.twig', [
             'controller_name' => 'TricksController',
         ]);
     }
