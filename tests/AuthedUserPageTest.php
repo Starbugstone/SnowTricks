d127719e3e24fd71e27e92d5537cbb48e2c8f9da
StarbugStone <pint2stone@gmail.com>
2019-01-27 00:55:29 +0100

    adding dmaicher/doctrine-test-bundle to avoid real database updates and ease testing

--- tests/AuthedUserPageTest.php
+++ tests/AuthedUserPageTest.php
@@ -18,9 +18,14 @@ class AuthedUserPageTest extends WebTestCase
 
     }
 
-    public function testCreatePage()
+    public function testCRUD()
     {
-        //going to a page that needs user auth
+
+        //----------------
+        // CREATE
+        //----------------
+
+        //going to the create page
         $crawler = $this->client->request('GET', '/trick/new');
 
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
@@ -34,20 +39,22 @@ class AuthedUserPageTest extends WebTestCase
 
         //TODO if already exists, then doesn't make so need to check else test is inefficient
 
-    }
+        //----------------
+        // EDIT
+        //----------------
 
-    public function testEditPage()
-    {
         //get the edit link
         $link = $this->goToSearchPageLink('create');
         //click the link to go to the edit page and check if available
         $this->client->click($link->link());
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
 
-    }
+        //TODO edit the page
 
-    public function testDeletePage()
-    {
+        //----------------
+        // DELETE
+        //----------------
+
         $link = $this->goToSearchPageLink('delete');
         $this->client->click($link->link());
         //$this->assertEquals(200, $this->client->getResponse()->getStatusCode());
@@ -62,7 +69,7 @@ class AuthedUserPageTest extends WebTestCase
 
         //get the 1st edit link
         $link = $crawler
-            ->filter('div.card:contains("php unit test")')
+            ->filter('div.card:contains("php unit test")') //get our test created article
             ->filter('a:contains(' . $linkText . ')')// find all links with the text
             ->eq(0) // select the 1st link in the list
         ;
