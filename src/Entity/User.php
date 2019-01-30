18f248e356cc8ed7f8fa38b4a40a74a2645364b5
StarbugStone <pint2stone@gmail.com>
2019-01-30 17:59:00 +0100

    Enabling login by mail or username. Adding an alphanum constraint on username to avoid the user registering an email address. Might update to regex to allow _ or -

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
@@ -40,6 +41,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\Type(type="alnum")
      */
     private $userName;
 
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -67,8 +67,13 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $credentials['email']]);
 
         if (!$user) {
-            // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Email could not be found.');
+            //try with userName
+            $user = $this->entityManager->getRepository(User::class)->findOneBy(['userName' => $credentials['email']]);
+            if(!$user){
+                // fail authentication with a custom error
+                throw new CustomUserMessageAuthenticationException('Email or username could not be found.');
+            }
+
         }
 
         return $user;
