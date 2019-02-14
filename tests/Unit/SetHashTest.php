f67ba746136813a548f360d9d24c5771e8843d38
StarbugStone <pint2stone@gmail.com>
2019-02-14 12:57:56 +0100

    Looking into some tests

---
+++ tests/Unit/SetHashTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Entity\User;
+use App\Security\UserSetHash;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+class SetHashTest extends WebTestCase
+{
+
+
+    public function testSetHash()
+    {
+        self::bootKernel();
+        $container = self::$kernel->getContainer();
+        $container = self::$container;
+
+        $userSetHash = self::$container->get(UserSetHash::class);
+
+        $user = new User();
+        $now = new \DateTime();
+
+        $hash = $userSetHash->set($user);
+        //testing if the hash is set and equals the returned hash
+        $this->assertEquals($hash, $user->getVerifiedHash(), 'the returned hash is incorrect');
+
+        //the set time should be now or a bit more
+        $this->assertGreaterThanOrEqual($now,$user->getVerifiedDateTime(), 'the set datetime is incorrect');
+
+    }
+}
