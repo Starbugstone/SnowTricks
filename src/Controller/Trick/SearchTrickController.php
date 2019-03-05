464bf2e547b26871d2c0897db122f248b54622cb
StarbugStone <pint2stone@gmail.com>
2019-03-05 10:22:26 +0100

    Adding basic search capabilities. Only searching on the title. could do better but will see if we have time for somthing more sofisticated

--- src/Controller/Trick/SearchTrickController.php
+++ src/Controller/Trick/SearchTrickController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controller\Trick;
 
+use App\FlashMessage\AddFlashTrait;
+use App\FlashMessage\FlashMessageCategory;
 use App\Repository\CategoryRepository;
 use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchTrickController extends AbstractController
 {
+    use AddFlashTrait;
+
     /**
      * @var TrickRepository
      */
@@ -25,13 +30,41 @@ class SearchTrickController extends AbstractController
     }
 
     /**
- * @Route("/search/{categoryId}", name="trick.search")
- */
+     * @Route("/search", name="trick.searchTrick", methods={"POST"})
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * Get the results from the search query
+     */
+    public function searchTrick(Request $request)
+    {
+        $submittedToken = $request->request->get('_token');
+
+        if ($this->isCsrfTokenValid('search-trick', $submittedToken)) {
+            $categories = $this->categoryRepository->findAll();
+            $searchTerm = $request->request->get('search_trick');
+            $tricks = $this->trickRepository->findBySearchQuery($searchTerm);
+            return $this->render('trick/search.html.twig', [
+                'tricks' => $tricks,
+                'categories' => $categories,
+            ]);
+        }
+
+        $this->addFlash(FlashMessageCategory::ERROR, "Bad CSRF Token");
+        return $this->redirectToRoute('home');
+
+    }
+
+    /**
+     * @Route("/search/{categoryId}", name="trick.search", methods={"GET"})
+     * @param string $categoryId
+     * @return \Symfony\Component\HttpFoundation\Response
+     * show tricks in category
+     */
     public function search($categoryId = "")
     {
         $categories = $this->categoryRepository->findAll();
         $criteria = array();
-        if($categoryId !== ""){
+        if ($categoryId !== "") {
             $criteria = array('category' => $categoryId);
         }
         $tricks = $this->trickRepository->findBy($criteria);
