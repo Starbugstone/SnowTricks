150f4800e1746b7d6325d69a13e8ef9546cf2170
StarbugStone <pint2stone@gmail.com>
2019-03-05 10:55:27 +0100

    Adding csrf protection to history reverting and using the redirect exception for CSRF Errors in revert history and serch tricks

--- src/Controller/Trick/Admin/RevertHistoryTrickController.php
+++ src/Controller/Trick/Admin/RevertHistoryTrickController.php
@@ -3,8 +3,12 @@
 namespace App\Controller\Trick\Admin;
 
 use App\Entity\Trick;
+use App\Exception\RedirectException;
+use App\FlashMessage\AddFlashTrait;
+use App\FlashMessage\FlashMessageCategory;
 use App\History\TrickHistory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
@@ -16,6 +20,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
  */
 class RevertHistoryTrickController extends AbstractController
 {
+    use AddFlashTrait;
 
     /**
      * @var TrickHistory
@@ -30,16 +35,24 @@ class RevertHistoryTrickController extends AbstractController
     /**
      * @param Trick $trick
      * @param $historyId
+     * @param Request $request
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
      * @Route("/trick/revert/{id}/{historyId}", name="trick.revert")
      */
-    public function revertHistory(Trick $trick, $historyId)
+    public function revertHistory(Trick $trick, $historyId, Request $request)
     {
-        $this->trickHistory->revertToHistory($trick->getId(), $historyId);
+        $submittedToken = $request->request->get('_token');
+        if (!$this->isCsrfTokenValid('revert-trick' . $historyId, $submittedToken)) {
+            throw new RedirectException($this->generateUrl('home'), 'Bad CSRF Token');
+        }
 
+        $this->trickHistory->revertToHistory($trick->getId(), $historyId);
+        $this->addFlash(FlashMessageCategory::SUCCESS, 'Reverted ' . $trick->getName());
         return $this->redirectToRoute('trick.show', [
             'id' => $trick->getId(),
             'slug' => $trick->getSlug(),
         ]);
+
+
     }
 }
