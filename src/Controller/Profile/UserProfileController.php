ac2a7a2aaf41da3d2093c99c291867e231c536e6
StarbugStone <pint2stone@gmail.com>
2019-03-06 15:05:36 +0100

    Starting to add the user profile admin page

--- src/Controller/Profile/UserProfileController.php
+++ src/Controller/Profile/UserProfileController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller\Profile;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UserProfileController extends AbstractController{
+/**
+ * Class UserProfileController
+ * @package App\Controller\Profile
+ * @IsGranted("ROLE_USER")
+ */
+class UserProfileController extends AbstractController
+{
 
     /**
      * @Route("/profile", name="admin.profile")
      */
-    public function index(){
+    public function index()
+    {
+        //Force login, we do not allow the remember me cookie for profile admin.
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        
         $user = $this->getUser();
         dd($user);
 
