47b2d218e3d7719eca5039e2468ea75ddd7ae521
StarbugStone <pint2stone@gmail.com>
2019-03-17 23:23:30 +0100

    moving the chip js to app.js. Adding some error checking + return if not used to chip and the select events. removing the accordeon as it is not used

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,14 +13,73 @@ require('../css/app.scss');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
-//init collapsable accordeon
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems);
+//init select elements
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('select');
+    if (elems.length === 0) {
+        return;
+    }
+    var instances = M.FormSelect.init(elems);
 });
 
-//init select elements
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems);
+//init chips
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.chips');
+    if (elems.length === 0) {
+        return;
+    }
+    let tagsData = document.getElementById('trick-tag-data');
+
+    //sanity checks and construct helpers
+    if (!tagsData) {
+        throw "missing the #trick-tag-data element that should contain all the chips data"
+    }
+
+    if (!("trickTagsJson" in tagsData.dataset) || !("allTagsJson" in tagsData.dataset)) {
+        throw "missing data-trick-tags-json or data-all-tags-json in the #trick-tag-data element. These elemens should contain all the json for set tags and all existing tags"
+    }
+
+
+    let trickTags = JSON.parse(tagsData.dataset.trickTagsJson);
+    let allTags = JSON.parse(tagsData.dataset.allTagsJson);
+
+    //creating the autocompleteOptions data object, needs to be of type { tag: null, tag2: null}
+    let allTagsObj = {};
+    allTags.map(
+        t => {
+            allTagsObj["'" + t.name + "'"] = null;
+            return allTagsObj;
+        }
+    );
+
+    var instances = M.Chips.init(elems, {
+        placeholder: "Add some tags",
+        secondaryPlaceholder: "keep adding Tags",
+        onChipAdd: chipInput,
+        onChipDelete: chipInput,
+        data: trickTags.map(function (tt) {
+            return {tag: tt.name} //mapping the name from json to tag: name
+        }),
+        autocompleteOptions: {
+            data: allTagsObj,
+            limit: Infinity,
+            minLength: 2
+        }
+    });
+
+    function chipInput() {
+        var elem = document.getElementById('chips1');
+        var tagInput = document.getElementById('trick_tags');
+        var instance = M.Chips.getInstance(elem);
+        var tagData = instance.chipsData;
+
+        var tags = [];
+        for (var $i = 0; $i < tagData.length; $i++) {
+            tags.push(tagData[$i].tag.toLowerCase());
+        }
+
+        let uniqTags = [...new Set(tags)];
+        tagInput.value = JSON.stringify(uniqTags);
+    }
+
 });
