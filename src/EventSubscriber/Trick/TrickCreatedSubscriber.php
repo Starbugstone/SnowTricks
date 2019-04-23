efd0f30ae0a3f1ecef9cdf9236f680777bac0272
StarbugStone <pint2stone@gmail.com>
2019-04-23 17:06:07 +0200

    As specified in the wireframe doc, when a new trick is submitted and has images, set the first image as primary

--- src/EventSubscriber/Trick/TrickCreatedSubscriber.php
+++ src/EventSubscriber/Trick/TrickCreatedSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace App\EventSubscriber\Trick;
 
+use App\Entity\Image;
 use App\Event\Trick\AbstractTrickEvent;
 use App\Event\Trick\TrickCreatedEvent;
 use App\Event\Trick\TrickEditedEvent;
@@ -10,6 +11,21 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class TrickCreatedSubscriber extends AbstractTrickSubscriber implements EventSubscriberInterface
 {
+
+    /**
+     * Setting the first image as primary as specified in the wireframes
+     * @param AbstractTrickEvent $event
+     */
+    public function setFirstImageAsPrimary(AbstractTrickEvent $event){
+        $trick = $event->getEntity();
+        if($trick->getImages()->count() >0){
+            /** @var Image $primImage */
+            $primImage = $trick->getImages()->first();
+            $primImage->setPrimaryImage(true);
+            $this->em->persist($primImage);
+            $this->em->flush();
+        }
+    }
 
     /**
      * Send trick to the database and set a flash message
@@ -27,9 +43,9 @@ class TrickCreatedSubscriber extends AbstractTrickSubscriber implements EventSub
      */
     public static function getSubscribedEvents()
     {
-        //TODO: set primary image
         return [
             TrickCreatedEvent::NAME => [
+                ['setFirstImageAsPrimary', 50],
                 ['registerTrickToDatabase', 30],
             ],
             TrickEditedEvent::NAME => [
