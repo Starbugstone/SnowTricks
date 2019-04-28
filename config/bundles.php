0a46f96cce23c2e644fce757a362c66057925e20
StarbugStone <pint2stone@gmail.com>
2019-04-28 23:49:16 +0200

    reverting the markdown removal from a52a0e0d3e166e89a720811aeb69a03684ecf7b3 as we will need the markdown if using same DB

--- config/bundles.php
+++ config/bundles.php
@@ -17,6 +17,7 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
